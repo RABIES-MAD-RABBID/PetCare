b560cb60ef303cc1819ae90fa0d74ffb69cc39d7
Fayo Halkano <fayojilohache@gmail.com>
2021-08-11 14:20:58 +0300

    first commit

---
+++ about.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+include ("partials/head.php");
+?>
+<body class="animsition">
+	<?php
+	include ("partials/header.php");
+
+
+
+?>
+
+	<!-- Title page -->
+	<section class="bg-img1 txt-center p-lr-15 p-tb-92" style="background-image: url('images/aboutus.jpg');">
+		<h2 class="ltext-105 cl0 txt-center">
+			About
+		</h2>
+	</section>	
+
+
+	<!-- Content page -->
+	<section class="bg0 p-t-75 p-b-120">
+		<div class="container">
+			<div class="row p-b-148">
+				<div class="col-md-7 col-lg-8">
+					<div class="p-t-7 p-r-85 p-r-15-lg p-r-0-md">
+						<h3 class="mtext-111 cl2 p-b-16">
+							Our Story
+						</h3>
+
+						<p class="stext-113 cl6 p-b-26">
+							We are a group of students from strathmore university persuing Bachelors Degree in Computer Science. This is our second year school project required by the highly accreddited exam over the same. We are passionate over what we do and the course we are persuing towards providing real time solutions to modern day problems.
+						</p>
+
+						<p class="stext-113 cl6 p-b-26">
+							Our goal is to satisfy the end user of our application.
+						</p>
+
+						<p class="stext-113 cl6 p-b-26">
+							Any questions? Let us know in store at Strathmore University, Ole Sangale Road, Opposite Siwaka, Room 10018 or call us on (+2540722000000) 50100 Nairobi Kenya.
+						</p>
+					</div>
+				</div>
+
+				<div class="col-11 col-md-5 col-lg-4 m-lr-auto">
+					<div class="how-bor1 ">
+						<div class="hov-img0">
+							<img src="images/about3.jpg" alt="IMG">
+						</div>
+					</div>
+				</div>
+			</div>
+			
+			<div class="row">
+				<div class="order-md-2 col-md-7 col-lg-8 p-b-30">
+					<div class="p-t-7 p-l-85 p-l-15-lg p-l-0-md">
+						<h3 class="mtext-111 cl2 p-b-16">
+							Our Mission
+						</h3>
+
+						<p class="stext-113 cl6 p-b-26">
+							Our mission is simple. Provide you with the finest solutions as per your needs.
+						</p>
+
+						<div class="bor16 p-l-29 p-b-9 m-t-22">
+							<p class="stext-114 cl6 p-r-40 p-b-11">
+								We care about you.
+							</p>
+
+							<span class="stext-111 cl8">
+								- Fayo Hache
+							</span>
+						</div>
+					</div>
+				</div>
+
+				<div class="order-md-1 col-11 col-md-5 col-lg-4 m-lr-auto p-b-30">
+					<div class="how-bor2">
+						<div class="hov-img0">
+							<img src="images/about2.jpg" alt="IMG">
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>	
+	
+		
+
+	<!-- Footer -->
+	<?php
+	include('partials/footer.php');
+	?>
+	
+</body>
+</html>
